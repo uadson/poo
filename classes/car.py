3a377f1ee0e55291e569bd2d10b832ad7e1568f6
uadson <uadsoncastro@gmail.com>
2020-10-29 07:54:13 -0300

    criado modulo meu_carro_eletrico.py herdando as classes do modulo car.py

--- classes/car.py
+++ classes/car.py
@@ -57,3 +57,53 @@ class Car():
 		else:
 			print('Você precisa abastecer!')
 			print('Tanque de combustível com capacidade para 40 litros.')
+
+
+
+class Bateria():
+	"""Uma tentativa simples de modelar uma bateria para um carro elétrico."""
+	def __init__(self, capacidade = 70):
+		"""Inicializa os atributos da bateria."""
+		self.capacidade = capacidade
+
+
+	def especificar_bateria(self):
+		"""Exibe uma frase que descreve a capacidade da bateria."""
+		print(f'Este carro tem uma bateria de capacidade de {self.capacidade}-KWh')
+
+
+	def obter_autonomia(self):
+		'''Exibe uma frase sobre a distância que o carro é capaz de percorrer
+		com essa bateria.'''
+		if self.capacidade == 70:
+			autonomia = 240
+		elif self.capacidade == 85:
+			autonomia = 270
+
+		message = f'''Este carro tem um autonomia de aproximadamente {autonomia}
+quilômetros com a carga da bateria completa.'''
+
+
+		print(message)
+
+
+	def atualizar_bateria(self):
+		if self.capacidade != 85:
+			self.capacidade = 85
+
+		print(f'Este carro tem uma bateria de capacidade de {self.capacidade}-KWh')
+
+		
+class EletricCar(Car):
+	""" Representa aspectos específicos de veículos elétricos."""
+	def __init__(self, marca, modelo, ano):
+		"""Inicializa os atributos da classe-pai."""
+		super().__init__(marca, modelo, ano)
+		self.bateria = Bateria()
+
+
+	def abastecer(self):
+		"""Um carro elétrico não precisa de um tanque de combustível."""
+		print('Este carro é elétrico e não precisa de combustível.')
+
+
