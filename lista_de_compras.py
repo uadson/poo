fb4deb9aabd9c47e4bf05f3abd00ba4f5bc0987b
uadson <uadsoncastro@gmail.com>
2021-01-19 17:36:54 -0300

    atributo complexo

---
+++ lista_de_compras.py
@@ -0,0 +1,28 @@
+class ListaCompras:
+
+    def __init__(self, *itens, nome=None):
+        self.nome = nome
+        self.itens = list(itens)
+
+if __name__ == '__main__':
+    objetoA = ListaCompras(nome='Arroz')
+    objetoB = ListaCompras(nome='Feijão')
+    objetoC = ListaCompras(nome='Carne')
+    objetoD = ListaCompras(nome='Tomate')
+    objetoE = ListaCompras(nome='Batata')
+
+    objetoLista = ListaCompras(
+        objetoA, 
+        objetoB, 
+        objetoC, 
+        objetoD, 
+        objetoE, 
+        nome='Lista de Compras'
+    )
+
+    print(objetoLista.nome)
+    
+    print()
+
+    for item in objetoLista.itens:
+        print(item.nome)
