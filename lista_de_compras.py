5f26358dbdf38b2f82030318c43c68a66bde93d4
uadson <uadsoncastro@gmail.com>
2021-01-29 16:13:54 -0300

    added staticmethod and classmethod

--- lista_de_compras.py
+++ lista_de_compras.py
@@ -1,8 +1,21 @@
 class ListaCompras:
+    forma_pagamento = {
+        1: 'dinheiro',
+        2: 'cartão',
+        3: 'bitcoin',
+    }
 
     def __init__(self, *itens, nome=None):
         self.nome = nome
         self.itens = list(itens)
+    
+    @staticmethod
+    def tabela():
+        return '-' * 42
+
+    @classmethod
+    def pagar(cls, select):
+        return cls.forma_pagamento.get(select)        
 
 if __name__ == '__main__':
     objetoA = ListaCompras(nome='Arroz')
@@ -20,12 +33,22 @@ if __name__ == '__main__':
         nome='Lista de Compras'
     )
 
-    print(objetoLista.nome)
-    
-    print()
+    print(ListaCompras.tabela())
 
+    print(objetoLista.nome)
     for item in objetoLista.itens:
         print(item.nome)
+
+    print(ListaCompras.tabela())
+    
+    print('Forma de Pagamento:')
+    for k, v in objetoLista.forma_pagamento.items():
+        print(k, v)
+    print(ListaCompras.tabela())
+    select = int(input('Selecione uma forma de pagamento: '))
+    print(f'Forma de pagamento selecionada: {ListaCompras.pagar(select)}')
+
+    print(objetoLista.tabela())
 
 '''
 output
